5682190043bfabb8d3d5badff82955f191b74524
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2024-12-09 18:00:35 -0300

    Update PlaceController.java

--- src/main/java/com/example/place_service/web/PlaceController.java
+++ src/main/java/com/example/place_service/web/PlaceController.java
@@ -5,6 +5,7 @@ import com.example.place_service.domain.PlaceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -19,7 +20,7 @@ public class PlaceController {
     }
 
     @PostMapping
-    public ResponseEntity<Mono<Place>> create(Place place) {
+    public ResponseEntity<Mono<Place>> create(@RequestBody Place place) {
         var createdPlace = placeService.create(place);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
     }
