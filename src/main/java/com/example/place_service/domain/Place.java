91aa81b8eba343aa3beefabe6a7a5edb780e49d5
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2024-12-09 01:30:44 -0300

    create record Place

---
+++ src/main/java/com/example/place_service/domain/Place.java
@@ -0,0 +1,6 @@
+package com.example.place_service.domain;
+
+import java.time.LocalDateTime;
+
+public record Place(Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+}
