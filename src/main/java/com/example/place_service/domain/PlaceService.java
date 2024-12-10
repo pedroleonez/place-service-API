e7a5f8b323bde3730915419de2404d42cdf4dfa9
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2024-12-09 22:08:51 -0300

    create generate slug

--- src/main/java/com/example/place_service/domain/PlaceService.java
+++ src/main/java/com/example/place_service/domain/PlaceService.java
@@ -1,19 +1,22 @@
 package com.example.place_service.domain;
 
 import com.example.place_service.api.PlaceRequest;
+import com.github.slugify.Slugify;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest) {
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()), placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
         return placeRepository.save(place);
     }
 }
