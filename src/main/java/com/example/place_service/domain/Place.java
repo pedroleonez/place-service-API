acd78fe245e203a6938f807e550f59f04731dcf9
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2024-12-09 21:40:58 -0300

    improving createdAt and updatedAt

--- src/main/java/com/example/place_service/domain/Place.java
+++ src/main/java/com/example/place_service/domain/Place.java
@@ -1,6 +1,9 @@
 package com.example.place_service.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import java.time.LocalDateTime;
 
-public record Place(Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record Place(Long id, String name, String slug, String state, @CreatedDate LocalDateTime createdAt, @LastModifiedDate LocalDateTime updatedAt) {
 }
