1ba28b4283db6e126b9a28f130f5a91798fb3cd5
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2024-12-11 02:00:54 -0300

    create tests

--- src/main/java/com/example/place_service/api/PlaceRequest.java
+++ src/main/java/com/example/place_service/api/PlaceRequest.java
@@ -1,6 +1,9 @@
 package com.example.place_service.api;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
-public record PlaceRequest(String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record PlaceRequest(
+        @NotBlank String name, @NotBlank String state) {
 }
--- src/main/java/com/example/place_service/web/PlaceController.java
+++ src/main/java/com/example/place_service/web/PlaceController.java
@@ -4,6 +4,7 @@ import com.example.place_service.api.PlaceRequest;
 import com.example.place_service.api.PlaceResponse;
 import com.example.place_service.domain.Place;
 import com.example.place_service.domain.PlaceService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,7 @@ public class PlaceController {
     }
 
     @PostMapping
-    public ResponseEntity<Mono<PlaceResponse>> create(@RequestBody PlaceRequest request) {
+    public ResponseEntity<Mono<PlaceResponse>> create(@Valid @RequestBody PlaceRequest request) {
         var placeResponse = placeService.create(request).map(PlaceMapper::fromPlaceToResponse);
         return ResponseEntity.status(HttpStatus.CREATED).body(placeResponse);
     }
